3b441dbc0a350541a8903ddc406a7d314a1fa36b
Scott Leberknight <scott.leberknight@nearinfinity.com>
2012-10-16 09:28:38 -0400

    Basic UI. Make delegate show and create the controller.

---
+++ Fortunes/FOViewController.h
@@ -0,0 +1,13 @@
+//
+//  FOViewController.h
+//  Fortunes
+//
+//  Created by Scott Leberknight on 10/16/12.
+//  Copyright (c) 2012 Scott Leberknight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FOViewController : UIViewController
+
+@end
---
+++ Fortunes/FOViewController.m
@@ -0,0 +1,32 @@
+//
+//  FOViewController.m
+//  Fortunes
+//
+//  Created by Scott Leberknight on 10/16/12.
+//  Copyright (c) 2012 Scott Leberknight. All rights reserved.
+//
+
+#import "FOViewController.h"
+
+@interface FOViewController ()
+
+@end
+
+@implementation FOViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
