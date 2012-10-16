cc60a17c987c5719b2c84c91cbe1f206ef48a175
Scott Leberknight <scott.leberknight@nearinfinity.com>
2012-10-16 09:22:08 -0400

    Initial Commit

---
+++ Fortunes/FOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FOAppDelegate.h
+//  Fortunes
+//
+//  Created by Scott Leberknight on 10/16/12.
+//  Copyright (c) 2012 Scott Leberknight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
